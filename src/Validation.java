b201c6ecb8b8a45a94b20465ff3f932386e638de
Sagar <sk9834334@gmail.com>
2021-12-30 19:47:23 +0500

    My Project

---
+++ src/Validation.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adnan
+ */
+class Validation {
+
+    static boolean isBlank(String PatientID) {
+         //To change body of generated methods, choose Tools | Templates.
+        return true;
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean PatientIDIsValid(String PatientID) {
+         //To change body of generated methods, choose Tools | Templates.
+        return true;
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean NameIsValid(String Name) {
+         //To change body of generated methods, choose Tools | Templates.
+        return true;
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean BloodGroupValid(String BloodGroup) {
+         //To change body of generated methods, choose Tools | Templates.
+        return true;
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean AgeIsValid(String Age) {
+        //To change body of generated methods, choose Tools | Templates.
+        return true;
+        //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean ContactNOIsValid(String ContactNO) {
+         //To change body of generated methods, choose Tools | Templates.
+        return true;
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean AddressValid(String Address) {
+         //To change body of generated methods, choose Tools | Templates.
+        return true;
+         //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static boolean HistoricDiseaseValid(String HistoricDisease) {
+        //To change body of generated methods, choose Tools | Templates.
+        return true;
+        //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
